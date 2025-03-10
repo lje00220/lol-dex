1dc863951891ce03b34ab2b4027598124c7db177
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-10 16:40:45 +0900

    feat: 공통 헤더 생성

---
+++ src/app/champions/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChampionPage = () => {
+  return <div>Champion Page</div>;
+};
+
+export default ChampionPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header></header>
+        <header>
+          <div className="flex flex-row justify-evenly items-center p-3 bg-slate-800 text-white">
+            <Link href={"/"}>홈</Link>
+            <Link href={"/champions"}>챔피언 목록</Link>
+            <Link href={"/items"}>아이템 목록</Link>
+            <Link href={"/rotation"}>챔피언 로테이션</Link>
+          </div>
+        </header>
         {children}
       </body>
     </html>
