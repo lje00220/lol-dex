76db7d734832df8a20127690815c26d0793a2fd2
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-11 17:24:11 +0900

    feat: 아이템 페이지 출력 기능

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { ItemWithId } from "@/types/Item";
+import Image from "next/image";
 
-const ItemPage = () => {
-  return <div>Item page</div>;
+const URL =
+  "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/item.json";
+
+const IMGURL = "https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item";
+
+const ItemPage = async () => {
+  const response = await fetch(URL, {
+    cache: "force-cache",
+  });
+
+  const { data } = await response.json();
+  const items: ItemWithId[] = Object.entries(data);
+  return (
+    <div className="px-5">
+      <h2 className="my-5 text-3xl font-extrabold text-blue-300">
+        아이템 목록
+      </h2>
+      <div className="grid grid-cols-6 gap-4">
+        {items.map((item) => {
+          const [id, itemDetail] = item;
+          return (
+            <div
+              key={id}
+              className="h-auto min-h-[250px] w-[180px] border-2 border-solid p-2"
+            >
+              <Image
+                src={`${IMGURL}/${id}.png`}
+                width={100}
+                height={100}
+                alt={itemDetail.name}
+                className="mx-auto my-2"
+              />
+              <div className="text-lg font-bold text-blue-600">
+                {itemDetail.name}
+              </div>
+              <div>{itemDetail.plaintext}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ItemPage;
