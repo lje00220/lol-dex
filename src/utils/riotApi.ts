b6f7519cb3ae0f377cd506984fd762f1bc6b343a
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-17 00:26:32 +0900

    refactor: router handler api 수정

--- src/utils/riotApi.ts
+++ src/utils/riotApi.ts
@@ -1,7 +1,7 @@
 import { ChampionWithId } from "@/types/champion";
 
 export const fetchRotations = async (): Promise<ChampionWithId[]> => {
-  const response = await fetch("http://localhost:3000/api/rotation");
+  const response = await fetch("/api/rotation");
   const data: ChampionWithId[] = await response.json();
   return data;
 };
