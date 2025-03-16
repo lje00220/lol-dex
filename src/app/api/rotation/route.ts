b512818d093e18692e46a198f5d2ac5daf4190db
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-16 23:15:20 +0900

    style: 헤더 반응형 제작

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -2,25 +2,22 @@ import { fetchChampionList } from "@/utils/serverApi";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    const response = await fetch(
-      "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
-      {
-        method: "GET",
-        headers: {
-          "X-Riot-Token": process.env.RIOT_API_KEY || "",
-        },
+  const response = await fetch(
+    "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
+    {
+      method: "GET",
+      headers: {
+        "X-Riot-Token": process.env.RIOT_API_KEY || "",
       },
-    );
+    },
+  );
 
-    const data = await response.json();
-    const rotation: number[] = data.freeChampionIds;
-    const championList = await fetchChampionList();
-    const result = rotation.map((r) =>
-      championList.find((c) => Number(c[1].key) === r),
-    );
-    return NextResponse.json(result);
-  } catch (error) {
-    console.log(error);
-  }
+  const data = await response.json();
+  const rotation: number[] = data.freeChampionIds;
+  const championList = await fetchChampionList();
+  const result = rotation?.map((r) =>
+    championList.find((c) => Number(c[1].key) === r),
+  );
+
+  return NextResponse.json(result);
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,32 @@
+"use client";
+
+import { Menu } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <header>
-      <div className="flex flex-row items-center justify-evenly bg-slate-800 p-3 text-white">
-        <Link href={"/"}>홈</Link>
-        <Link href={"/champions"}>챔피언 목록</Link>
-        <Link href={"/items"}>아이템 목록</Link>
-        <Link href={"/rotation"}>챔피언 로테이션</Link>
+    <header className="bg-slate-800 p-4 text-lg text-white">
+      <div className="flex">
+        <nav className="hidden w-full justify-evenly gap-6 md:flex">
+          <Link href="/">홈</Link>
+          <Link href="/champions">챔피언 목록</Link>
+          <Link href="/items">아이템 목록</Link>
+          <Link href="/rotation">챔피언 로테이션</Link>
+        </nav>
+        <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+          <Menu size={28} />
+        </button>
       </div>
+      {isOpen && (
+        <nav className="mt-3 flex flex-col items-center gap-4 md:hidden">
+          <Link href="/">홈</Link>
+          <Link href="/champions">챔피언 목록</Link>
+          <Link href="/items">아이템 목록</Link>
+          <Link href="/rotation">챔피언 로테이션</Link>
+        </nav>
+      )}
     </header>
   );
 };
